8cd48a06bec75d6951fbd9550ff1fc2f6c0b58ea
FindingLaptop <168267851+FindingLaptop@users.noreply.github.com>
2025-01-09 00:32:29 +0700

    Update klasifikasi_dataset.py
    Hanya membuat comment

--- klasifikasi_dataset.py
+++ klasifikasi_dataset.py
@@ -25,27 +25,36 @@ df = data.copy()
 df.head()
 df
 
+# Menghapus kolom 'Pregnancies'
 df = pd.DataFrame(data.drop('Pregnancies', axis=1))
 df
 
+# Mendeskripsikan DataFrame
 df.describe(include='all')
 
+# Informasi DataFrame
 df.info()
 
+# Normalisasi data
 scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 dff = scaler.fit_transform(df)
 dff = pd.DataFrame(dff, columns=['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI','DiabetesPedigreeFunction', 'Age', 'Outcome'])
 
+# Membagi Data menjadi Fitur dan Target
 X = pd.DataFrame(dff.drop('Outcome', axis=1))
 Y = dff['Outcome'].values.reshape(-1, 1)
 
+# Membagi Data menjadI Data Latih dan Data Uji
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.2, random_state=0)
 
+# Membuat dan Melatih Model
 model = GaussianNB()
 model.fit(X_train, Y_train.ravel())
 
+# Memprediksi Data uji
 y_pred = model.predict(X_test)
 
+# Menghitung dan Menampilkan akurasi
 from sklearn import metrics
 print("\33[43m Accuracy Is:", metrics.accuracy_score(Y_test, y_pred))
 
